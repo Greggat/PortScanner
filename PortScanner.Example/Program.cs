82b842e13dab2a4f7ff10efa2da50538185a2e83
Greggat <greggat96@gmail.com>
2020-03-29 10:39:33 -0500

    Initial Commit

---
+++ PortScanner.Example/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace PortScanner.Example
+{
+    class Program
+    {
+        static void OnOpenPortScanned(ushort port)
+        {
+            Console.WriteLine($"Open Port: {port}");
+        }
+
+        static void OnClosedPortScanned(ushort port)
+        {
+            Console.WriteLine($"Closed Port: {port}");
+        }
+
+        static void Main(string[] args)
+        {
+            PortScanner scanner = new PortScanner("google.com", 1, 65535);
+            scanner.OpenPortScanned += OnOpenPortScanned;
+            scanner.ClosedPortScanned += OnClosedPortScanned;
+
+            scanner.ScanAsync();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ PortScanner/PortScanner.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace PortScanner
+{
+    public class PortScanner
+    {
+        public delegate void ScannedPortEventHandler(ushort port);
+
+        public event ScannedPortEventHandler ClosedPortScanned;
+        public event ScannedPortEventHandler OpenPortScanned;
+        public event ScannedPortEventHandler FailedPortScanned;
+
+        public delegate void ScannerFinishedEventHandler();
+        public event ScannedPortEventHandler ScannerFinished;
+
+        private List<ushort> _openPorts;
+        private List<ushort> _closedPorts;
+
+        private string _host;
+        private ushort _scanPortMin;
+        private ushort _scanPortMax;
+
+        private ushort _portsScanned;
+
+        public PortScanner(string host, ushort rangeMin, ushort rangeMax)
+        {
+            if (rangeMin > rangeMax)
+                throw new ArgumentException($"rangeMind({rangeMin}) can not be higher than rangeMax({rangeMax})!");
+
+            _host = host;
+            _scanPortMin = rangeMin;
+            _scanPortMax = rangeMax;
+        }
+
+        public void Scan()
+        {
+            _openPorts = new List<ushort>();
+            _closedPorts = new List<ushort>();
+
+            for (ushort i = _scanPortMin; i <= _scanPortMax; i++)
+            {
+                CheckPort(i);
+            }
+        }
+
+        private void CheckPort(ushort port)
+        {
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                socket.Connect(_host, port);
+
+                //Connect succeeded
+                _openPorts.Add(port);
+                OpenPortScanned?.Invoke(port);
+            }
+            catch (SocketException e)
+            {
+                if (e.SocketErrorCode == SocketError.ConnectionRefused ||
+                    e.SocketErrorCode == SocketError.TimedOut)
+                {
+                    _closedPorts.Add(port);
+                    ClosedPortScanned?.Invoke(port);
+                }
+                else if (e.SocketErrorCode == SocketError.AccessDenied)
+                {
+                    _openPorts.Add(port);
+                    OpenPortScanned?.Invoke(port);
+                }
+                else
+                {
+                    Console.Write("Unhandled SocketError: ");
+                    Console.Write(Enum.GetName(typeof(SocketError), e.SocketErrorCode));
+                    Console.Write("\n");
+                }
+            }
+            finally
+            {
+                if (socket.Connected)
+                    socket.Disconnect(false);
+
+                socket.Close();
+            }
+        }
+
+        public void ScanAsync()
+        {
+            _openPorts = new List<ushort>();
+            _closedPorts = new List<ushort>();
+
+            for (ushort i = _scanPortMin;i <= _scanPortMax;i++)
+            {
+                //Fire and forget?
+                CheckPortAsync(i);
+            }
+            Console.WriteLine("here");
+        }
+
+        private async Task CheckPortAsync(ushort port)
+        {
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                await socket.ConnectAsync(_host, port);
+
+                //Connect succeeded
+                _openPorts.Add(port);
+                OpenPortScanned?.Invoke(port);
+            }
+            catch (SocketException e)
+            {
+                if (e.SocketErrorCode == SocketError.ConnectionRefused ||
+                    e.SocketErrorCode == SocketError.TimedOut)
+                {
+                    _closedPorts.Add(port);
+                    ClosedPortScanned?.Invoke(port);
+                }
+                else if (e.SocketErrorCode == SocketError.AccessDenied)
+                {
+                    _openPorts.Add(port);
+                    OpenPortScanned?.Invoke(port);
+                }
+                else
+                {
+                    Console.Write("Unhandled SocketError: ");
+                    Console.Write(Enum.GetName(typeof(SocketError),e.SocketErrorCode));
+                    Console.Write("\n");
+                }
+            }
+            finally
+            {
+                if(socket.Connected)
+                    socket.Disconnect(false);
+
+                socket.Close();
+            }
+        }
+    }
+}
